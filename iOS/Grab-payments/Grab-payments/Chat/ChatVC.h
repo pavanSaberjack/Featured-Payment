ffdc4d123301cc811feda0347f9d5bd3cc58a1e4
Pavan <itagi75@gmail.com>
2019-12-07 13:08:04 +0530

    Added Chat UI library

---
+++ iOS/Grab-payments/Grab-payments/Chat/ChatVC.h
@@ -0,0 +1,17 @@
+//
+//  ChatVC.h
+//  Grab-payments
+//
+//  Created by Pavan on 07/12/19.
+//  Copyright © 2019 Pavan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChatVC : NOCChatViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS/Grab-payments/Grab-payments/Chat/ChatVC.m
@@ -0,0 +1,27 @@
+//
+//  ChatVC.m
+//  Grab-payments
+//
+//  Created by Pavan on 07/12/19.
+//  Copyright © 2019 Pavan. All rights reserved.
+//
+
+#import "ChatVC.h"
+
+@implementation ChatVC
+// Overrides these three methods below to provide basic classes.
++ (Class)cellLayoutClassForItemType:(NSString *)type
+{
+    // ...
+}
+
++ (Class)inputPanelClass
+{
+    // ...
+}
+
+- (void)registerChatItemCells
+{
+    // ...
+}
+@end
