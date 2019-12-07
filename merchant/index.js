6191b032de78cc5b497b5b28156e7e879bb1d5ab
X1 <x1@Kislays-MBP.Dlink>
2019-12-08 00:13:35 +0530

    route corrected

--- merchant/index.js
+++ merchant/index.js
@@ -1,35 +1,39 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const axios = require('axios');
 const targetBaseUrl = 'http://localhost:3000/';
+const router = express.Router();
 
 const API_OTP = "https://www.google.com";
 const API_CON = "https://www.google.com";
 
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`), router);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
 
-app.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
   res.sendFile(`${__dirname}/public/index.html`);
 });
 
 
 
 //Request for OTP Initation//
-app.post('/confirm', (req, res) => {
+router.get('/confirm', (req, res) => {
+
+  
   
   axios.get(API_OTP)
   .then(response => {
     console.log(response.data.url);
     console.log(response.data.explanation);
-    res.render('/confirmationPage.html')
-    // res.redirect('confirmationPage.html');
+    // res.sendFile(path.join(__dirname+'/public/confirmationPage.html'));
+    res.sendFile((`${__dirname}/public/confirmationPage.html`));
   })
   .catch(error => {
     console.log(error);
@@ -38,7 +42,7 @@ app.post('/confirm', (req, res) => {
 });
 
 
-app.post('/otpClicked', (req, res) => {
+router.post('/otpClicked', (req, res) => {
   
   axios.get(API_CON)
   .then(response => {
@@ -53,6 +57,6 @@ app.post('/otpClicked', (req, res) => {
 });
 
 
-app.post('/ntClicked', (req, res) => {
+router.post('/ntClicked', (req, res) => {
   res.redirect(targetBaseUrl + `index.html`);
 });
--- merchant/public/sp.js
+++ merchant/public/sp.js
@@ -1,17 +1,5 @@
 
-const button2 = document.getElementById('ntButton');
-button.addEventListener('click', function(e) {
-  console.log('button was clicked');
-
-  fetch('/ntClicked', {method: 'POST'})
-    .then(function(response) {
-      if(response.ok) {
-        console.log('Click was recorded');
-        return;
-      }
-      throw new Error('Request failed.');
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
-});
+// const button2 = document.getElementById('ntButton');
+// button2.onclick = function (req,res) {
+//     document.getElementById("ntButton").submit();
+// }
